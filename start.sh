05612c4436b4cbd3a459fe0d5a1c43547d1232a2
meiranbot <87614073+meiranbot@users.noreply.github.com>
2023-12-06 17:34:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/Lucifer.git /Lucifer
+  git clone https://github.com/meiranbot/Lucifer.git /Lucifer
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucifer
